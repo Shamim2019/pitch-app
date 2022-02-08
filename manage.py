da3647b9aa976471d8955335d73f148c7de3c402
Shamim <shamimmaloba@gmail.com>
2022-02-08 10:55:10 +0300

    downvotes and upvotes

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
-from flask_script import Manager, Server
-from flask_migrate import Migrate, MigrateCommand
+from flask import Manager, Server
+from flask  import Migrate, MigrateCommand
 from app import create_app,db
 from app.models import User
 
